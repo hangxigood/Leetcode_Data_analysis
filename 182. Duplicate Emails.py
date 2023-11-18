4d347b72e663c0abf9b0771f6e911bc2adc15556
Hangxi Xiang <hangxigood@gmail.com>
2023-11-17 23:07:46 -0700

    question 182 pandas solution

--- 182. Duplicate Emails.py
+++ 182. Duplicate Emails.py
@@ -1,36 +1,43 @@
-+-------------+---------+
-| Column Name | Type    |
-+-------------+---------+
-| id          | int     |
-| email       | varchar |
-+-------------+---------+
-id is the primary key (column with unique values) for this table.
-Each row of this table contains an email. The emails will not contain uppercase letters.
+# +-------------+---------+
+# | Column Name | Type    |
+# +-------------+---------+
+# | id          | int     |
+# | email       | varchar |
+# +-------------+---------+
+# id is the primary key (column with unique values) for this table.
+# Each row of this table contains an email. The emails will not contain uppercase letters.
  
 
-Write a solution to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
+# Write a solution to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
 
-Return the result table in any order.
+# Return the result table in any order.
 
-The result format is in the following example.
+# The result format is in the following example.
 
  
 
-Example 1:
+# Example 1:
 
-Input: 
-Person table:
-+----+---------+
-| id | email   |
-+----+---------+
-| 1  | a@b.com |
-| 2  | c@d.com |
-| 3  | a@b.com |
-+----+---------+
-Output: 
-+---------+
-| Email   |
-+---------+
-| a@b.com |
-+---------+
-Explanation: a@b.com is repeated two times.
+# Input: 
+# Person table:
+# +----+---------+
+# | id | email   |
+# +----+---------+
+# | 1  | a@b.com |
+# | 2  | c@d.com |
+# | 3  | a@b.com |
+# +----+---------+
+# Output: 
+# +---------+
+# | Email   |
+# +---------+
+# | a@b.com |
+# +---------+
+# Explanation: a@b.com is repeated two times.
+
+# Pandas Solution
+import pandas as pd
+
+def duplicate_emails(person: pd.DataFrame) -> pd.DataFrame:
+    data = person[person.duplicated(subset=['email'])]['email'].unique()
+    return pd.DataFrame({"Email":data})
